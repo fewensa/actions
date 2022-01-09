579ec740c5127addd5dc6f745081254d3925efec
fewensa <fewensa@protonmail.com>
2022-01-09 18:25:18 +0800

    ci

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,10 @@ set -e
 REPOSITORY=$1
 ACCESS_TOKEN=$2
 SSH_KEY=$3
-TARGET_NAME=$4
+SSH_HOST=$4
+TARGET_NAME=$5
 
-PATH_SSH=$PWD/.ssh
+PATH_SSH=$HOME/.ssh
 
 mkdir -p $PATH_SSH
 
@@ -15,6 +16,11 @@ if [ -n "$SSH_KEY" ]; then
   echo "$SSH_KEY" > $PATH_SSH/id_rsa
 fi
 
+if [ -n "$SSH_HOST" ]; then
+  apk update
+  apk add openssh
+  ssh-keyscan github.com >> ~/.ssh/known_hosts
+fi
 
 Z_REPOSITORY=$REPOSITORY
 
@@ -27,6 +33,7 @@ if [ -n "$ACCESS_TOKEN" ]; then
       Z_REPOSITORY=$(echo $REPOSITORY | sed -e "s/http:\/\//http:\/\/${ACCESS_TOKEN}@/g")
   fi
 fi
+
 
 
 if [ -z "$TARGET_NAME" ]; then
