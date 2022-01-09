05d2c97257ed48fae0617c69f1526e07886c9650
fewensa <fewensa@protonmail.com>
2022-01-09 18:09:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+Clone Any Git Repo Action
+===
+
+
+## Clone a repository via https
+
+```
+- name: Clone repository
+  uses: fewensa/clone-any-action@v1
+  with:
+    repository: 'https://github.com/org/repo'
+```
+
+## Clone a repository via ssh
+
+```
+- name: Clone repository
+  uses: fewensa/clone-any-action@v1
+  with:
+    repository: 'git@github.com:org/repo.git'
+    ssh-key: |
+      ---PRIVATE KEY---
+      ...
+      ---END---
+```
+
---
+++ entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+REPOSITORY=$1
+ACCESS_TOKEN=$2
+SSH_KEY=$3
+TARGET_NAME=$4
+
+PATH_SSH=$PWD/.ssh
+
+mkdir -p $PATH_SSH
+
+if [ -n "$SSH_KEY" ]; then
+  echo "$SSH_KEY" > $PATH_SSH/id_rsa
+fi
+
+
+Z_REPOSITORY=$REPOSITORY
+
+if [ -n "$ACCESS_TOKEN" ]; then
+  echo 'access token: ' $ACCESS_TOKEN
+  if [[ "$REPOSITORY" =~ ^https://* ]]; then
+      Z_REPOSITORY=$(echo $REPOSITORY | sed -e "s/https:\/\//https:\/\/${ACCESS_TOKEN}@/g")
+  fi
+  if [[ "$REPOSITORY" =~ ^http://* ]]; then
+      Z_REPOSITORY=$(echo $REPOSITORY | sed -e "s/http:\/\//http:\/\/${ACCESS_TOKEN}@/g")
+  fi
+fi
+
+
+if [ -z "$TARGET_NAME" ]; then
+    git clone $Z_REPOSITORY
+
+    echo "Cloned $2 repository successfully."
+else
+    git clone Z_REPOSITORY $TARGET_NAME
+
+    echo "Cloned $2 repository successfully."
+    echo "Access the repository content using \"cd $2\"."
+fi
+
