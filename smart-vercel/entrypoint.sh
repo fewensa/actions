3321e1b3de85a7f9d33d31c88705c2e170cd7fb6
fewensa <fewensa@protonmail.com>
2022-02-08 12:44:12 +0800

    nvm use node

--- smart-vercel/entrypoint.sh
+++ smart-vercel/entrypoint.sh
@@ -3,9 +3,10 @@
 
 set -e
 
-NODE_VERSION=${INPUT_NODE_VERSION}
+NODE_VERSION=${INPUT_NODE_VERSION:-14.17.0}
 
-nvm install ${NODE_VERSION:-14.17.0}
+nvm install ${NODE_VERSION}
+nvm use ${NODE_VERSION}
 
 node -v
 npm -v
